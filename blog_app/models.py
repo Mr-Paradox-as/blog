b1c05c4364de80bc62d3d1dc6ad48df45a8f3d2e
Abhishek Shakya <abhishekshakya@Abhisheks-MacBook-Air.local>
2024-02-12 11:17:18 +0530

    post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -6,7 +6,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    
 class Post(models.Model):
     title    = models.CharField(max_length=100)
     content  = models.TextField()
